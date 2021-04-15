d1a0791ffee267146eba5888fe5a1dde625926a7
Luis Martires <luis.martires@outlook.pt>
2021-04-15 14:06:11 +0100

    done

--- index.js
+++ index.js
@@ -1,18 +1,15 @@
 const mongoose = require('mongoose');
-
-// Import of the model Recipe from './models/Recipe.model.js'
 const Recipe = require('./models/Recipe.model');
-// Import of the data from './data.json'
 const data = require('./data');
 
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
-// Connection to the database "recipe-app"
 mongoose
   .connect(MONGODB_URI, {
     useCreateIndex: true,
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    useFindAndModify: false
   })
   .then(self => {
     console.log(`Connected to the database: "${self.connection.name}"`);
@@ -21,7 +18,52 @@ mongoose
   })
   .then(() => {
     // Run your code here, after you have insured that the connection was made
+    updateDB()
+
   })
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+async function updateDB() {
+
+  try {
+
+    // CREATE
+
+    let recipe = await Recipe.create ({ title: "Mixed Eggs", level: "UltraPro Chef", ingredients: ["Eggs", "Milk", "Salt"], cuisine: "French", dishType: "breakfast", duration: 120, creator: "Chef  Luís Mártires"})
+    console.log(`Recipe Created: ${recipe.title}`);
+
+    let otherRecipes = await Recipe.insertMany(data);
+    otherRecipes.forEach((recipes) => {
+    console.log(`Other Recipes: ${recipes.title}`);
+    });
+
+    // READ
+
+    let recipesfromDB = await Recipe.find();
+    console.log(`All Recipes from DB ${recipesfromDB}`);
+ 
+    // UPDATE
+
+    await Recipe.findOneAndUpdate( {title: "Rigatoni alla Genovese"}, {duration: 100});
+    console.log("Recipe Updated Successfully");
+
+    // DELETE
+
+    await Recipe.deleteOne( {title: "Carrot Cake"});
+    console.log("Recipe Deleted Successfully");
+  }
+
+  catch(e) {
+    console.log("Error Occurred");
+  }
+
+  finally {
+    mongoose.connection.close();
+  }
+}
+
+
+
